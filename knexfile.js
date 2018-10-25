5cc7783d4888e54e2322a40f34e1b2e064fca19d
Micah Weiss <micahjweiss@gmail.com>
2018-10-25 17:33:28 -0500

    Added database schema, migration, and seeds.

---
+++ knexfile.js
@@ -0,0 +1,28 @@
+// Update with your config settings.
+console.log('Migration file called');
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/cryptex',
+    migrations: {
+      directory: __dirname + '/database/migrations',
+    },
+    seeds:{
+      directory: __dirname + '/database/seeds'
+    }
+  },
+
+
+  production: {
+    client: 'pg',
+    connection: 'postgres://localhost/cryptex', //production will need some deployement in the tutorial: process.env.DATABASE_URL
+    migrations: {
+      directory: __dirname + '/database/migrations',
+    },
+    seeds:{
+      directory: __dirname + '/database/seeds/production'
+    }
+  },
+
+};
